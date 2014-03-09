0e3024c4d10a669911c1db6a1eb75e6525b09509
retalemine <email.subhu@gmail.com>
2014-03-09 20:33:31 +0530

    slf4j with log4j impl

--- mvnweb-vaadin/src/main/java/in/retalemine/AppUI.java
+++ mvnweb-vaadin/src/main/java/in/retalemine/AppUI.java
@@ -1,16 +1,22 @@
 package in.retalemine;
 
 import in.retalemine.component.BillingComponent;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.UI;
 
 public class AppUI extends UI {
 
 	private static final long serialVersionUID = -4411379836207492227L;
+	private static final Logger logger = LoggerFactory.getLogger(AppUI.class);
 
 	@Override
 	protected void init(VaadinRequest request) {
 		setContent(new BillingComponent());
+		logger.info("Completed setting layout");
 	}
 
 }
